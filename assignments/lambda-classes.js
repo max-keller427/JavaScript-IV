263401dfbdc827be5d6d71dfe1edfc6f7077f091
max-keller427 <max.keller427@gmail.com>
2019-01-24 00:04:11 -0700

    making progress on classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,22 +11,24 @@ class Person  {
     }
 }
 
-class Instructor {
+class Instructor extends Person {
     constructor (value) {
+        super (value);
         this.specialty = value.specialty;
         this.favLanguage = value.favLanguage;
         this.catchPhrase = value.catchPhrase;
     }
     demo (subject) {
-        return `Today we are learning about ${this.subject}`
+        return `Today we are learning about ${this.specialty}.`
     }
     grade (student, subject) {
-        return `${this.name} revieves a perfect score on ${this.subject}`
+        return `${student.name} revieves a perfect score on ${subject}.`
     }
 }
 
-class Student {
+class Student extends Person{
     constructor (value) {
+        super (value);
         this.previousBackground = value.previousBackground;
         this.className = value.className;
         this.favSubjects = value.favSubjects;
@@ -35,15 +37,16 @@ class Student {
         return listsSubjects.call(this.favSubjects);
     }
     PRAssignment (subject) {
-        return `${this.student} has submitted a PR for ${this.subject}`;
+        return `${this.name} has submitted a PR for ${subject}`;
     }
     sprintChallenge (subject) {
-        return `${this.student} has begun sprint challenge on ${this.subject}`;
+        return `${this.student} has begun sprint challenge on ${this.subject}.`;
     }
 }
 
-class ProjectManager {
+class ProjectManager extends Instructor {
     constructor (value) {
+        super (value);
         this.gradClassName = value.gradClassName;
         this.favInstructor = value.favInstructor;
     }
@@ -67,7 +70,7 @@ const cam = new Instructor ({
 
 const max = new Student ({
     name: 'Max Keller',
-    location: 'Utah',
+    location: 'Dallas, TX',
     age: '26',
     gender: 'M',
     previousBackground: 'Climbing rocks',
@@ -84,3 +87,8 @@ const jeffe = new ProjectManager ({
     favInstructor: 'Patrick K.',
     })
 
+console.log(jeffe.speak());
+console.log(cam.demo());
+console.log(cam.grade(max, "HTML"))
+console.log(max.favSubjects);
+console.log(max.PRAssignment('CSS'));
